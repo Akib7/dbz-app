fd9628a6254d057aef2f021a3aff1e8c74e52fec
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-27 16:28:48 +0600

    added directory, and character components in Homepage

--- src/components/character/character.component.jsx
+++ src/components/character/character.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./character.styles.scss";
 
-const Character = ({ _name, imageUrl, linkUrl }) => (
+const Character = ({ title, imageUrl, linkUrl }) => (
   <div className="character">
     <div
       className="background-image"
@@ -12,9 +12,30 @@ const Character = ({ _name, imageUrl, linkUrl }) => (
     />
 
     <div className="content">
-      <h1 className="title">{_name.toUpperCase()}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
     </div>
   </div>
 );
 
 export default Character;
+
+// import React from "react";
+
+// import "./character.styles.scss";
+
+// const MenuItem = ({ title, imageUrl, size }) => (
+//   <div className={`${size} menu-item`}>
+//     <div
+//       className="background-image"
+//       style={{
+//         backgroundImage: `url(${imageUrl})`,
+//       }}
+//     />
+//     <div className="content">
+//       <h1 className="title">{title.toUpperCase()}</h1>
+//       <span className="subtitle">SHOP NOW</span>
+//     </div>
+//   </div>
+// );
+
+// export default MenuItem;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import Character from "../character/character.component";
 
 import "./directory.styles.scss";
@@ -10,28 +11,28 @@ class Directory extends React.Component {
     this.state = {
       sections: [
         {
-          _name: "Goku",
-          ImageUrl:
+          title: "Goku",
+          imageUrl:
             "https://theouterhaven.b-cdn.net/wp-content/uploads/2022/03/goku-dragon-ball-guru-824x490-1-750x400.jpg",
           id: 1,
           linkUrl: "goku",
         },
         {
-          _name: "Vegeta",
-          ImageUrl: "https://i.redd.it/81i19fa6ng871.jpg",
+          title: "Vegeta",
+          imageUrl: "https://i.redd.it/81i19fa6ng871.jpg",
           id: 2,
           linkUrl: "vegeta",
         },
         {
-          _name: "Gohan",
-          ImageUrl:
+          title: "Gohan",
+          imageUrl:
             "https://dragonball.guru/wp-content/uploads/2021/03/toppng.com-mystic-gohan-dragon-ball-z-gohan-1021x2859-1-e1617011806816-400x400.png",
           id: 3,
           linkUrl: "gohan",
         },
         {
-          _name: "Trunks",
-          ImageUrl: "https://images2.alphacoders.com/772/thumb-1920-772447.jpg",
+          title: "Trunks",
+          imageUrl: "https://images2.alphacoders.com/772/thumb-1920-772447.jpg",
           id: 4,
           linkUrl: "trunks",
         },
@@ -42,9 +43,9 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherProps }) => {
-          <Character key={id} {...otherProps} />;
-        })}
+        {this.state.sections.map(({ id, ...otherProps }) => (
+          <Character key={id} {...otherProps} />
+        ))}
       </div>
     );
   }
