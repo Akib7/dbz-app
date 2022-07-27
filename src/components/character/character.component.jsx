95a3013780cc8c680448a1c1ee2547437046adda
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-27 21:53:39 +0600

    added the logo, and created routes

--- src/components/character/character.component.jsx
+++ src/components/character/character.component.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+
 import "./character.styles.scss";
 
-const Character = ({ title, imageUrl, linkUrl }) => (
-  <div className="character">
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return <Component {...props} router={{ location, navigate, params }} />;
+  }
+
+  return ComponentWithRouterProp;
+}
+
+const Character = ({ title, imageUrl, linkUrl, router }) => (
+  <div
+    className="character"
+    onClick={() => router.navigate(`${router.location.pathname}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -17,25 +33,4 @@ const Character = ({ title, imageUrl, linkUrl }) => (
   </div>
 );
 
-export default Character;
-
-// import React from "react";
-
-// import "./character.styles.scss";
-
-// const MenuItem = ({ title, imageUrl, size }) => (
-//   <div className={`${size} menu-item`}>
-//     <div
-//       className="background-image"
-//       style={{
-//         backgroundImage: `url(${imageUrl})`,
-//       }}
-//     />
-//     <div className="content">
-//       <h1 className="title">{title.toUpperCase()}</h1>
-//       <span className="subtitle">SHOP NOW</span>
-//     </div>
-//   </div>
-// );
-
-// export default MenuItem;
+export default withRouter(Character);
