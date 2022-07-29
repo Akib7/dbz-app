7b7458f08f4cc11c5ac3a18216527f663b1fda9d
Akib Ahmed <akibahmed7360@gmail.com>
2022-07-29 22:43:00 +0600

    changed some stylings

--- src/pages/goku/goku.component.jsx
+++ src/pages/goku/goku.component.jsx
@@ -14,8 +14,19 @@ class Goku extends React.Component {
           imageUrl:
             "https://p325k7wa.twic.pics/high/dragon-ball/dragonball-project-z/05-characters/dbzk_goku.png?twic=v1/cover=390x546/step=10/quality=80",
           id: 1,
-          description:
-            "A Saiyan warrior born on Planet Vegeta and sent to Earth as a baby.\nHis Saiyan name is Kakarot, but he was given the name Goku by his adoptive grandfather Gohan, from whom he also learned martial arts.\nAfter Gohan's passing, Goku spent his days alone in the mountains until visited by a young girl in search of the mystical Dragon Balls...",
+          description: (
+            <>
+              <p>
+                A Saiyan warrior born on Planet Vegeta and sent to Earth as a
+                baby. <br /> His Saiyan name is Kakarot, but he was given the
+                name Goku by his adoptive grandfather Gohan, from whom he also
+                learned martial arts. <br />
+                After Gohan's passing, Goku spent his days alone in the
+                mountains until visited by a young girl in search of the
+                mystical Dragon Balls...
+              </p>
+            </>
+          ),
         },
       ],
     };
